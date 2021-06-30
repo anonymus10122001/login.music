fa5d04a1934b6afb9f7a1bdd75593eba77860c11
Nguyễn Văn Huy <84430968+anonymus10122001@users.noreply.github.com>
2021-07-01 03:02:22 +0700

    Delete web.js

--- web.js
+++
@@ -1,7 +0,0 @@
-var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-  if (!isChrome){
-      $('#iframeAudio').remove()
-  }
-  else {
-      $('#playAudio').remove()
-  }
